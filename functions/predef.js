375a35b3534bece27f8bb9155bdde73c4b516a5f
Kemunto Gift <giftoyugi@gmail.com>
2022-10-14 09:52:14 +0300

    first set of predefined functions added

---
+++ functions/predef.js
@@ -0,0 +1,33 @@
+//Eval method
+// evaluates code represented as a string
+let a = 10;
+let d = 20;
+let text = "a* d";
+let result = eval(text);
+console.log(result);
+
+// isFinite
+// returns boolean
+let number = isFinite(123);
+
+//isNaN
+// returns boolean
+isNaN(123);
+isNaN("2005/12/12");
+//result 1 is false, 2 is true
+
+// parseFloat
+//Parses a string and returns a floating point number or first number
+parseFloat(10);
+parseFloat("10");
+parseFloat("10.33");
+parseFloat("34 45 66");
+parseFloat("Michael has 40 dogs");
+// returns, 10, 10, 10.33, 34, NaN
+
+//parseInt
+//parses a string argument and returns an integer of the specified radix
+parseInt("010");
+parseInt("10", 8);
+parseInt("0x10");
+//returns 10, 8, 16
